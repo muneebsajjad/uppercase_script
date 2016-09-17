edb591dd001f34b66016425b341d59cced318e55
Muneeb Sajjad <muneebsajjad@gmail.com>
2016-09-17 15:01:37 +0500

    added a new comment

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
-
+// muneeb added this comment
 /** @testString string [test string] */
-	$testString = "Qbc.-_&&$@^&^(&)45461454*&*&^&$#!@#$AbC";
-	$result     =	testUpperCase($testString);
-	echo $result;
+    $testString = "Qbc.-_&&$@^&^(&)45461454*&*&^&$#!@#$AbC";
+    $result = testUpperCase($testString);
+    echo $result;
 
-/**
- * [testUpperCase check if string start with the upper case letter]
- * @param  [string] $str [pass test string]
- * @return [type]      [string]
- */
-	function testUpperCase($str){
-		// test if pattren matches
-		if(preg_match('/^[A-Z][a-z0-9]/', $str)){
-		 	return "Uppercase in the string starting";
-		}else{
-			return "No uppercase in the string starting";
-		}
-	}
-
-?>
+    /**
+     * [testUpperCase check if string start with the upper case letter].
+     *
+     * @param [string] $str [pass test string]
+     *
+     * @return [type] [string]
+     */
+    function testUpperCase($str)
+    {
+        // test if pattren matches
+        if (preg_match('/^[A-Z][a-z0-9]/', $str)) {
+            return 'Uppercase in the string starting';
+        } else {
+            return 'No uppercase in the string starting';
+        }
+    }
